ca1fb06cfb0211349889d7a5e2487bf63023deb9
lizhangxin <2528526196@qq.com>
2020-11-07 09:10:19 +0800

    asd

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -58,35 +58,55 @@ class WeixinController extends Controller
         }
         echo 'access_token'.$token;
     }
-    public function responseMsg(){
-        $poststr = file_get_contents("php://input");
-        Log::info('======'.$poststr);
-        $postarray = simplexml_load_string($poststr);
-        if ($postarray->MsgType=='event'){
-            if ($postarray->Event=='subscribe'){
-                $array = ['关注成功','你好','zzzz'];
-                $Content = $array[array_rand($array)];
-                $this->infocodl($postarray,$Content);
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)){
+
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $type = $postObj->MsgType;
+            $customrevent = $postObj->Event;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+       <ToUserName><![CDATA[%s]]></ToUserName>
+       <FromUserName><![CDATA[%s]]></FromUserName>
+       <CreateTime>%s</CreateTime>
+       <MsgType><![CDATA[%s]]></MsgType>
+       <Content><![CDATA[%s]]></Content>
+       <FuncFlag>0</FuncFlag>
+       </xml>";
+            if($type=="event" and $customrevent=="subscribe"){
+                $contentStr = "感谢你的关注\n回复1查看联系方式\n回复2查看最新资讯\n回复3查看法律文书";
+                $msgType = "text";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
             }
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                if($keyword=="1"){
+                    $contentStr = "qiphon";}
+                if($keyword=="2"){
+                    $contentStr = "test 。";}
+                if($keyword=="3"){
+                    $contentStr = "test333";}
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+
+        }else {
+            echo "";
+            exit;
         }
-
     }
-    public function infocodl($postarray,$Content){
-        $ToUserName =$postarray->FromUserName;
-        $FromUserName = $postarray->ToUserName;
-        $time=time();
-        $text ="text";
-        $ret = '<xml>
-                    <ToUserName><![CDATA[%s]]></ToUserName>
-                    <FromUserName><![CDATA[%s]]></FromUserName>
-                    <CreateTime>%s</CreateTime>
-                    <MsgType><![CDATA[%s]]></MsgType>
-                    <Content><![CDATA[%s]]></Content>
-                </xml>';
-        $info = sprintf($ret,$ToUserName,$FromUserName,$time,$text,$Content);
-        echo $info;
-    }
-
 }
 
 
