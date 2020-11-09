e203b49bb1e5e28cf5c35ca302cb0ec7370be829
lizhangxin <2528526196@qq.com>
2020-11-09 18:52:40 +0800

    123

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -99,9 +99,10 @@ class WeixinController extends Controller
         if($weather['success']){
             $content = '';
             foreach($weather['result'] as $v){
-                $content .='日期:'.$v['days'].$v['week'].'当日温度:'.$v['temperature'].'天气:'.$v['weather'].'风向:'.$v['wind'];
+                $content  .= '地区:'.$v['citynm'].'日期:'.$v['days'].$v['week'].'当日温度:'.$v['temperature'].'天气:'.$v['weather'].'风向:'.$v['wind'];
             }
         }
+        echo $content;die;
         return $content;
     }
     //文本回复消息
