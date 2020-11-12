a6dce55b9c52513b32c320073df3ef957a54974c
lizhangxin <2528526196@qq.com>
2020-11-12 13:49:34 +0800

    rk

--- app/UserModel.php
+++ app/UserModel.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserModel extends Model
 {
-    //
+    protected  $table = "user";
+    public $timestamps = false;
+    protected  $guarded = [];
 }
